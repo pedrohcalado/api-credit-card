a05c4f842e379d77c5af7d2cb86678c62aac2bb7
pedrohcalado <pedrocalado22@gmail.com>
2021-05-26 19:26:08 -0300

    Cria estrutura base dos repositories

---
+++ Repositories/CreditCardRepository.cs
@@ -0,0 +1,15 @@
+namespace CreditCardGenerator.Repositories
+{
+  public class CreditCardRepository
+  {
+    public void GetCreditCardsByUser()
+    {
+
+    }
+
+    public void SaveCreditCard()
+    {
+
+    }
+  }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+namespace CreditCardGenerator.Repositories
+{
+  public class UserRepository
+  {
+    public void Save()
+    {
+
+    }
+  }
+}
