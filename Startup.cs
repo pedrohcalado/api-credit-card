d90dfef023d437cf6044a5032c17e89e00bdf87b
pedrohcalado <pedrocalado22@gmail.com>
2021-05-28 18:41:00 -0300

    Aplica injecao de dependencia e uso do data context

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CreditCardGenerator.Data;
+using CreditCardGenerator.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +35,12 @@ namespace CreditCardGenerator
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CreditCardGenerator", Version = "v1" });
             });
+
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+
+            services.AddScoped<DataContext, DataContext>();
+            services.AddTransient<CreditCardRepository, CreditCardRepository>();
+            services.AddTransient<UserRepository, UserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
