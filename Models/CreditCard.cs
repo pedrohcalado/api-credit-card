4116d7cc59c91c5684a552697439d6ba069f3062
pedrohcalado <pedrocalado22@gmail.com>
2021-05-28 18:39:12 -0300

    Finaliza criacao dos models

--- Models/CreditCard.cs
+++ Models/CreditCard.cs
@@ -1,12 +1,22 @@
 using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CreditCardGenerator.Models
 {
   public class CreditCard
   {
+    public CreditCard(string number, string email)
+    {
+      Number = number;
+      Email = email;
+      CreatedAt = DateTime.Now;
+    }
+    [Key]
     public int Id { get; private set; }
-    public int Number { get; private set; }
-    public User User { get; private set; }
+    public string Number { get; private set; }
+    [Required(ErrorMessage = "The email address is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; private set; }
+    public DateTime CreatedAt { get; private set; }
   }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,17 @@
-using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CreditCardGenerator.Models
 {
   public class User
   {
+    public User(string email)
+    {
+      Email = email;
+    }
+    [Key]
     public int Id { get; private set; }
-    public IEnumerable<CreditCard> CreditCards { get; private set; }
+    [Required(ErrorMessage = "The email address is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; private set; }
   }
 }
