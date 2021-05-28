8a3dc3bccd6811ee303be90223b70d7e3f280761
pedrohcalado <pedrocalado22@gmail.com>
2021-05-28 18:41:50 -0300

    Finaliza controller

--- Controllers/CreditCardController.cs
+++ Controllers/CreditCardController.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CreditCardGenerator.Models;
 using CreditCardGenerator.Repositories;
+using CreditCardGenerator.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CreditCardGenerator.Controllers
@@ -8,21 +13,52 @@ namespace CreditCardGenerator.Controllers
   public class CreditCardController : Controller
   {
     private readonly CreditCardRepository _creditCardRepository;
-    public CreditCardController(CreditCardRepository creditCardRepository)
+    private readonly UserRepository _userRepository;
+    public CreditCardController(CreditCardRepository creditCardRepository, UserRepository userRepository)
     {
       _creditCardRepository = creditCardRepository;
+      _userRepository = userRepository;
     }
 
     [HttpGet]
-    public string GetCreditCards([FromQuery] string email)
+    public async Task<ActionResult<IEnumerable<CreditCard>>> GetCreditCardsByUser([FromQuery] string email)
     {
-      return "Ok";
+      var creditCards = await _creditCardRepository.GetCreditCardsByUser(email);
+      return Ok(creditCards);
     }
 
     [HttpPost]
-    public string CreateCreditCard([FromQuery] string email)
+    public async Task<ActionResult<CreditCard>> CreateCreditCard([FromBody] User userData)
     {
-      return email;
+      if(!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      try
+      {
+        var creditCardNumber = RandomNumberService.GenerateRandomCreditCardNumber();
+        var newCreditCard = new CreditCard(creditCardNumber, userData.Email);
+        await _creditCardRepository.SaveCreditCard(newCreditCard);
+
+        var user = await _userRepository.Get(userData.Email);
+
+        if (user == null)
+        {
+          var newUser = new User(userData.Email);
+          await _userRepository.Save(newUser);
+        }
+
+        return Ok(new {
+          message = "Credit card created successfully",
+          creditCardNumber = newCreditCard.Number,
+          }
+        );
+      }
+      catch
+      {
+        return BadRequest(new {message = "There was a problem creating the card"});
+      }
     }
   }
 }
